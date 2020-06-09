57d3a4bab27b007bf84bfdcb55c75e31e81efa45
Matéo <mateo.borlat@gmail.com>
2020-06-09 12:06:37 +0200

    pancarte add

--- menu.js
+++ menu.js
@@ -30,13 +30,13 @@ function createSceneMenu()
 
 
     // Voir Nico
-    // var spPanneau = new BABYLON.SpriteManager("spPanneau", "resources/pancarte.png", 2, 128, scene, 0.01, BABYLON.Texture.NEAREST_SAMPLINGMODE);
+    var spPanneau = new BABYLON.SpriteManager("spPanneau", "resources/pancarte.png", 0, 128, scene, 0.01, BABYLON.Texture.NEAREST_SAMPLINGMODE);
     
-    // var spritePanneau = new BABYLON.Sprite("spritePanneau", spPanneau);
-    // spritePanneau.playAnimation(1,4, true, 300);
-    // spritePanneau.height = 4;
-    // spritePanneau.width = 4;
-    // spritePanneau.position = new BABYLON.Vector3(7,6.5,0);
+    var spritePanneau = new BABYLON.Sprite("spritePanneau", spPanneau);
+    spritePanneau.playAnimation(1,4, true, 300);
+    spritePanneau.height = 4;
+    spritePanneau.width = 4;
+    spritePanneau.position = new BABYLON.Vector3(7,6.5,0);
 
     var end;
     var spawn;
